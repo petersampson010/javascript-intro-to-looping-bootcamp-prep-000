470a704034f0d6feff1263139d0118f36ca8cc8b
Peter Sampson <petersampson010@gmail.com>
2019-08-12 18:04:43 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,31 +1,30 @@
 
-
 function forLoop(array) {
-  for (let i = `0`; i < `25`; i++) {
-    if (i == `1`) {
+  for (let i=0; i<25; i++) {
+    if(i===1) {
       array.push(`I am ${i} strange loop.`)
     } else {
-      array.push(`I am ${i} strange loops.`) }
+      array.push(`I am ${i} strange loops.`)
+    }
   }
-  return array 
+  return array
 }
 
 function whileLoop(n) {
-  while (n>`0`)
-  {console.log(n--)}
+  while (n>0) {
+    console.log(n--)
+  }
   return "done"
 }
 
-var i = `0`;
-
+var i=1
 function incrementVariable() {
-  i = i + `1`;
+  i = i + 1;
 }
 
 function doWhileLoop(array) {
-  do { 
-    array.pop()
-  } while (array.length > `0` && incrementVariable())
+  do { array.pop() 
+  } while (array.length>0)
   return array
 }
-  
+
